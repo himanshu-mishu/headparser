9f8860528f9394317b98e7412d34d047327236fc
himanshu-mishu <himanshu.agrl111@gmail.com>
2025-05-31 10:01:11 +0530

    headparser

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 // index.js
 // where your node app starts
 
-// init project
-require('dotenv').config();
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
+var cors = require("cors");
+
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// Enable CORS for FCC testing
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// Serve static files from /public
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Basic homepage route
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// Test API route
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-// listen for requests :)
+// ✅ Header Parser Endpoint
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
+// Start the server
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
